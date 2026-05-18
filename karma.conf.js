34372e0252bb3be2e4c3dfc42046e7008dfccc80
Antoine Dumeige <antoine@antoines-mbp-2.home>
2026-05-18 21:39:56 +0200

    fix for macos, plus generate karama repoort

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,33 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-process.env.CHROME_BIN = require('puppeteer').executablePath()
+const fs = require('fs');
+const path = require('path');
+
+function resolveChromePath() {
+  const candidates = [
+    process.env.CHROME_BIN,
+    '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary',
+    '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser'
+  ];
+
+  const directPath = candidates.find((candidate) => candidate && fs.existsSync(candidate));
+  if (directPath) {
+    return directPath;
+  }
+
+  try {
+    return require('puppeteer').executablePath();
+  } catch (error) {
+    return null;
+  }
+}
+
+const resolvedChromePath = resolveChromePath();
+if (resolvedChromePath) {
+  process.env.CHROME_BIN = resolvedChromePath;
+}
 
 module.exports = function (config) {
   config.set({
@@ -12,6 +38,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
+      require('karma-junit-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
@@ -27,19 +54,33 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/client'),
+      dir: path.join(__dirname, './coverage/client'),
       subdir: '.',
       reporters: [
         { type: 'html' },
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
+    junitReporter: {
+      outputDir: path.join(__dirname, 'target', 'surefire-reports'),
+      outputFile: 'karma-results.xml',
+      useBrowserName: false
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox'
+        ]
+      }
+    },
+    browsers: ['ChromeHeadlessCI'],
     singleRun: false,
     restartOnFileChange: true
   });
